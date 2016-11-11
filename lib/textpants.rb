6889843c0957818b75303762051acb575204f83b
ofk <ofkjpn+github@gmail.com>
2016-11-11 14:14:19 +0900

    Add textpants module

--- lib/textpants.rb
+++ lib/textpants.rb
@@ -1,5 +1,19 @@
-require 'textpants/version'
+require 'nkf'
 
 module Textpants
-  # Your code goes here...
+  module_function
+
+  def sanitize(input)
+    NKF.nkf('-WwLum0', input.to_s.scrub('?').gsub(/[\u0000-\u0008\u000E-\u001F\u007F]/, ''))
+  end
+
+  def normalize(input)
+    NKF.nkf('-WwZ0Z1Xm0', sanitize(input))
+  end
+
+  def squish(input)
+    normalize(input).gsub(/[\s\p{Cf}\p{Zs}\p{Zp}\p{Zl}]+/, ' ').strip
+  end
 end
+
+require 'textpants/version'
--- test/textpants_test.rb
+++ test/textpants_test.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'test_helper'
 
 class TextpantsTest < Minitest::Test
@@ -5,7 +6,18 @@ class TextpantsTest < Minitest::Test
     refute_nil ::Textpants::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_sanitize
+    assert_equal '', Textpants.sanitize('')
+    assert_equal 'abc', Textpants.sanitize("a\u0000b\u007Fc")
+    assert_equal "  a\nA\nあ\n亞　！", Textpants.sanitize("  a\nA\rあ\r\n亞　！")
+  end
+
+  def test_normalize
+    assert_equal '', Textpants.normalize('')
+    assert_equal "  a\nA\nあ\n亞 !", Textpants.normalize("  a\nA\rあ\r\n亞　！")
+  end
+
+  def test_squish
+    assert_equal 'a A あ 亞 !', Textpants.squish(" \u0000 a \n A \r あ \r\n 亞　！\uFFFB")
   end
 end
